ad4bd6939c1fe78dae2f66d57163c5836bd2d224
Wolfgang Zdrenka <wzdrenka@gmail.com>
2018-12-05 20:56:42 +1300

    Initial Commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by Wolfgang Zdrenka on 5/12/18.
+//  Copyright © 2018 Wolfgang Zdrenka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
